39f9efe771015ad092517c83caa1aaf5d6e5d124
Eckhard Schlemm <eckhard@Ixion>
2018-11-24 17:21:57 +0100

    Updated preprocess script

--- preprocess.sh
+++ preprocess.sh
@@ -2,31 +2,30 @@
 
 #. /usr/share/fsl/5.0/etc/fslconf/fsl.sh
 
-BASEDIR=/mnt/data/WakeUpNemo_demo/
+BASEDIR=/mnt/data/ToposNemo
 
-for VISIT in V0 V3; do
 
-	LESIONDIR=$BASEDIR/$VISIT
-	cd $LESIONDIR
-	DIR=$LESIONDIR/86
+
+	
+	cd $BASEDIR
+	DIR=$BASEDIR/86
 
 	mkdir -p $DIR
-	cp -r ./original_masks/* $DIR
+	cp -r ./lesionmasks/* $DIR
 
 	for f in $(ls -d $DIR/*); do
-		gunzip $f
+		# gunzip $f
  		g=$(basename $f)
  		g=${g%.*} # *.nii
  		g=${g%.*}
  		mkdir $DIR/$g
  		mv $DIR/${g}.nii $DIR/$g/
 
- 		fslroi "$DIR/${g}/${g}.nii" "$DIR/${g}/${g}_cropped.nii" 6 181 6 217 2 181
+ 		fslroi "$DIR/${g}/${g}.nii" "$DIR/${g}/${g}_cropped.nii" 0 181 0 217 0 181
  		gunzip "$DIR/${g}/${g}_cropped.nii"
 	done
 
-	cp -r $DIR $LESIONDIR/116
+	cp -r $DIR $BASEDIR/116
 
-done
 
 exit
